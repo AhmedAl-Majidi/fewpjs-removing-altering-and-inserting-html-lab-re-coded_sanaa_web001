cbca90f61ff8242ee3d81f1b1d6a3d7fd730f74e
Ahmed Al-Majidi <af774857999@gmail.com>
2020-08-05 08:40:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let main = document.getElementById('main');
 main.remove();
 
-let newHeader = document.createElement('h1');
+let newHeader = document.createElement('h1#victory');
 document.body.appendChild(newHeader);
 newHeader.innerHTML = 'Ahemd is the champion';
